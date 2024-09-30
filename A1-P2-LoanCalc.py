1b5d7c08008c3fe872c09ae1203b48e89e16be9a
Your Name <w0402993@nscc.ca>
2024-09-30 13:18:12 -0300

    Finished A1 P2

--- A1-P2-LoanCalc.py
+++ A1-P2-LoanCalc.py
@@ -4,11 +4,24 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    #Display opening message
+    print("Weekly Loan Calculator")
 
+    #Prompt user for loan amount
+    loanAmount = float(input("\nEnter the amount of loan: "))
 
+    #Prompt user for interest rate
+    interestRate = float(input("Enter the interest rate (%): "))
+    
+    #Prompt user for the number of years
+    loanYears = float(input("Enter the number of years: "))
 
+    #Math to calculate the weekly payments
+    i = interestRate / 5200
 
+    weeklyPayment = (i / (1-(1 + i)**-(52 * loanYears))) * loanAmount
 
+    print("\nYour weekly payment will be: ${0:,.2f}".format(weeklyPayment))
 
 
     # YOUR CODE ENDS HERE
